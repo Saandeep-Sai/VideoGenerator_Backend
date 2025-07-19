25c63106ac042bcff617e0135f1f7dad615dc6ab
Saandeep-Sai <saandeepsaiturpu@gmail.com>
2025-07-19 17:21:50 +0530

    Run worker an combined-v3

--- video_gen/urls.py
+++ video_gen/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
+
 
 urlpatterns = [
+    path("", health_check),  # So HEAD / returns 200 OK
     path('api/', include('generator.urls')),
 ]
